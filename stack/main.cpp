af5f54a21a6462e05e90e4e42fd5dc8012fda37c
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-01-06 07:48:24 +0200

    Tested out the generic array stack

--- stack/main.cpp
+++ stack/main.cpp
@@ -1,12 +1,24 @@
 #include "arrayStack.h"
 #include "iostream"
+#include "genericArrayStack.h"
 using namespace std;
 
-int main() {
+int main()
+{
     arrayStack s(3);
+    genericArrayStack<int> gs(10);
+
+    cout << "This is where we're doing the array stack imp \n";
     s.push(10);
     s.push(20);
-    cout << s.getTop() << "\n"; 
+    cout << s.getTop() << "\n";
     s.pop();
-    cout << s.getTop() << "\n"; 
+    cout << s.getTop() << "\n";
+
+    cout << "This is where we're doing the generic array stack imp \n";
+    gs.push(10);
+    gs.push(20);
+    cout << gs.getTop() << "\n";
+    gs.pop();
+    cout << gs.getTop() << "\n";
 }
