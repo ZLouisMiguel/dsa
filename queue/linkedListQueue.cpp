84d780581cdc2e6e9c9918586c78d9846e1fdabd
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-01-21 09:47:23 +0200

    Added basic addition , removal and traversal operations

--- queue/linkedListQueue.cpp
+++ queue/linkedListQueue.cpp
@@ -36,4 +36,51 @@ public:
     {
         return size == 0;
     }
+
+    void enqueue(int el)
+    {
+        Node *n = new Node(el);
+        if (head == nullptr)
+        {
+            head = n;
+            tail = n;
+        }
+        else
+        {
+            tail->next = n;
+            tail = n;
+        }
+        size++;
+    }
+
+    void dequeue()
+    {
+        if (isEmpty())
+            return;
+
+        Node *temp = head;
+        head = head->next;
+        temp->next = nullptr;
+        delete temp;
+        size--;
+    }
+
+    void display()
+    {
+        Node *temp = head;
+
+        while (temp != nullptr)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
+
+    // void display2() {
+    //     while(!q.isEmpty()) {
+    //         cout << q.front() << "\n";
+    //         q.dequeue();
+    //     }
+    // }
+    
 };
