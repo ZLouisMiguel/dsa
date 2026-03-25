36b6ba9d5e3774030e68ab38f82350df5341321e
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-03-25 21:18:57 +0200

    Recursion practice question

---
+++ recursion/exercise1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int fun ( int n ) {
+    if(n == 0) return 1;
+    else return 7 + fun(n-2);
+}
+
+int main() {
+
+    cout<< fun(4) <<endl; //output = 15;
+    return 0;
+}
