9b78ab6d218daa6a3e685969936812eea2e76fa4
ZLouisMiguel <zigirumugabelouismiguel@gmail.com>
2026-04-25 22:47:00 +0200

    random patterns in cpp

---
+++ randomQns/patterns.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // for(int i = 0 ; i < 10 ; i++) {
+    //     for(int j = i ; j < 10 ; j++) {
+    //         cout<<"*";
+    //     }
+    //     cout<<"\n";
+    // }
+
+
+    for(int i = 0 ; i < 10 ; i++) {
+        for(int j = 0 ; j < i ; j++) {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    
+}
